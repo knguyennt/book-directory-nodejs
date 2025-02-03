adce85c876e30b7a4eda48196ceaf72824313697
nguyennguyentruong <nguyenntk.18@grad.uit.edu.vn>
2025-02-03 23:25:37 +0700

    feat: add basic layout for collection

--- App.js
+++ App.js
@@ -3,7 +3,10 @@ import bookRouter from './routes/books.js'
 
 const app = express();
 
-app.use(express.static('public'))
+app.use('/public', express.static('public'))
+app.use('/css', express.static('./node_modules/bootstrap/dist/css'));
+app.use('/js', express.static('./node_modules/bootstrap/dist/js'));
+
 
 app.use(bookRouter)
 
